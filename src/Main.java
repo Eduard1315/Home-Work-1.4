404046212e1935ecd21a6ed6449f651d986b3f8a
rosso <rossosh2004@mail.ru>
2022-08-09 19:37:14 +0300

    Home-Work-1.4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println();
+
+        //Task 1
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        int a = 10;
+        while (a >= 1) {
+            System.out.print(a + " ");
+            a--;
+        }
+        System.out.println();
+        System.out.println();
+
+
+        //Task 2
+        int fraidy = 5;
+        System.out.println("Сегодня Пятница " + fraidy + "-е число. Необходимо подготовить отчет");
+        System.out.println();
+        for (int b = 0; b < 3; b++) {
+            if (fraidy <= 31) {
+                fraidy = fraidy + 7;
+            }
+            System.out.println("Сегодня Пятница" + fraidy + "-е число. Необходимо подготовить отчет");
+            System.out.println();
+
+            //Третью не написал, т.к. её решение перемешивается со вторым заданием.
+            }
     }
 }
